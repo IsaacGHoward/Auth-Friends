78c133c39b72435513e8477a06c188a143907113
IsaacGHoward <howardisaac100@gmail.com>
2021-01-15 17:50:28 -0800

    Private Route works, login validates properly, and logout works

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { PrivateRoute } from './Components/PrivateRoute';
 
-import Login from './Components/Login'
+import Login from './Components/Login';
+import FriendsList from './Components/FriendsList';
 
 import axios from 'axios';
 
@@ -13,10 +15,15 @@ function App() {
   }
   return (
     <Router>
-      <Link to="/login">Login</Link>
-      <Link onClick={logout}>Logout</Link>
+      <Link to="/login">Login </Link>
+      <Link onClick={logout}>Logout </Link>
+      <Link to="/friendslist">Friends List </Link>
       <Switch>
-        <Route path="/login" component={Login}></Route>
+        <PrivateRoute exact path="/friendslist">
+          <FriendsList/>
+        </PrivateRoute>
+        <Route path="/login" component={Login}/>
+        <Route component={Login} />
       </Switch>
     </Router>
   );
---
+++ friends/src/Components/FriendsList.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function FriendsList(props){
+  return(
+    <div>
+      PRIVATE FRIENDS LIST
+    </div>
+  )
+}
